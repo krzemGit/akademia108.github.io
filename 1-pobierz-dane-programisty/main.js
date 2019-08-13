a5b85d372141d39ef9303f86a42760e3960560c7
Lech Krzem <lechkrzeminski@gmail.com>
2019-08-13 17:27:05 +0200

    corrected ajax exercise

--- 1-pobierz-dane-programisty/main.js
+++ 1-pobierz-dane-programisty/main.js
@@ -5,7 +5,6 @@ fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php
     $('button').click( function () { 
         $('div').empty()
         for (let i = 0; i < Object.keys(employeeData).length; i++) {
-            console.log("pętla działa")
             $('div').append(`<p>${Object.keys(employeeData)[i]}:  ${Object.values(employeeData)[i]}</p>`); }
     })
 })
